b473f2d00708eda4d3b88167dcad6f12e1c8c587
Jesse <jessetyoungblood@gmail.com>
2020-04-15 23:01:58 -0400

    Initial Commit

---
+++ SimpleHMACAuth/SimpleHMACAuth.h
@@ -0,0 +1,19 @@
+//
+//  SimpleHMACAuth.h
+//  SimpleHMACAuth
+//
+//  Created by Jesse T Youngblood on 4/15/20.
+//  Copyright © 2020 Jesse Youngblood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleHMACAuth.
+FOUNDATION_EXPORT double SimpleHMACAuthVersionNumber;
+
+//! Project version string for SimpleHMACAuth.
+FOUNDATION_EXPORT const unsigned char SimpleHMACAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleHMACAuth/PublicHeader.h>
+
+
